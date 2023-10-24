e22d2cf6ad2a9b9ba2f987a2daf39c1710b77051
josemigg <8917610+josemigg@users.noreply.github.com>
2023-10-24 16:01:29 +0200

    Create basics.js

---
+++ todo/basics.js
@@ -0,0 +1,39 @@
+/*
+ 1. Crea una función que compruebe si un número está entre 
+*/
+
+/*
+  Arregla el error
+*/
+
+const firstName = Jose Miguel;
+const lastName = González García;
+
+
+/*
+  Arregla el error
+*/
+
+const x = 2;
+const y = 4;
+x = y + x;
+
+/*
+  Cambia la declaración de variables usando let o const
+*/
+
+var myVar1 = 1;
+var myVar2 = 25;
+var sum = myVar1 + myVar2;
+sum = sum +1;
+
+
+/*
+  Declara una variable que contenga un número, otra que contenga el texto 'Hola' y otra que contenga el texto "Hola [NUMERO]" donde el [NUMERO] será el valor de la primera variable.
+*/ 
+
+
+/*
+ Declara una variable con una cadena de texto y escribe en la consola el texto "Mi cadena de texto tiene [NUMERO] caracteres", donde [NUMERO] será el tamaño de esa cadena de texto. 
+ Por ejemplo, si la cadena de texto es "hola", el resultado debe ser "Mi cadena de texto tiene 4 caracteres".
+*/
