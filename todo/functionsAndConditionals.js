3e84cc71d1254950467d448a7ae679620a4fe65b
josemigg <8917610+josemigg@users.noreply.github.com>
2023-10-24 20:46:34 +0200

    Create functionsAndConditionals.js

---
+++ todo/functionsAndConditionals.js
@@ -0,0 +1,22 @@
+/*
+  Crear una función que me diga si un número está entre 0 y 10  
+   => utilizar un return;
+*/
+
+/*
+  Crea una función que me diga si un parámetro es de tipo cadena de texto o número.
+*/
+
+
+/*
+  Crea una función que me diga si una cadena de texto tiene el valor 'pending'. Debe ser case insensitive, es decir,
+  me dirá "true" en todas las variantes mayúsculas/minúsculas de este texto: 'Pending', 'PEnding', 'PeNdIng', etc.
+*/
+
+
+
+/*
+  Crea una función que simule una compra con tarjeta de crédito. Tendrá tres parámetros: 
+    - El precio del producto
+    - gastadoEsteMes, 
+*/
