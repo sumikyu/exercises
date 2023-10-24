e377d625b64d9b788cf92cfa37461544554d4788
josemigg <8917610+josemigg@users.noreply.github.com>
2023-10-24 20:53:16 +0200

    Update functionsAndConditionals.js

--- todo/functionsAndConditionals.js
+++ todo/functionsAndConditionals.js
@@ -14,9 +14,12 @@
 */
 
 
-
 /*
   Crea una función que simule una compra con tarjeta de crédito. Tendrá tres parámetros: 
     - El precio del producto
-    - gastadoEsteMes, 
+    - El dinero gastado este mes
+    - El límite mensual
+  Si me queda dinero suficiente para comprar el producto, la función debe devolver el dinero gastado este
+  mes incluyendo el precio del producto. Si no puedo comprar el producto pq el dinero no me da, debe escribir 
+  en pantalla un mensaje diciendo que no se puede comprar por llegar al límite mensual.
 */
