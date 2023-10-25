20e135097cad0d0d4763b500677522f6ec21e539
josemigg <8917610+josemigg@users.noreply.github.com>
2023-10-25 16:09:23 +0200

    Update functionsAndConditionals.js

--- todo/functionsAndConditionals.js
+++ todo/functionsAndConditionals.js
@@ -23,3 +23,49 @@
   mes incluyendo el precio del producto. Si no puedo comprar el producto pq el dinero no me da, debe escribir 
   en pantalla un mensaje diciendo que no se puede comprar por llegar al límite mensual.
 */
+
+
+/*
+  Crea una función que indique si un número es par o impar.
+*/
+
+/*
+  Crea una función que indique el mayor de dos números
+*/
+
+/*
+  Crea una función que indique el mayor de tres números
+*/
+
+
+/*
+  Crea una función que, dados dos edades, te diga cuántos años hay entre ellas
+*/
+
+/*
+ Crea una función que al pasarle la nota de un examen (de 0 a 10) te devuelva la nota: suspenso, aprobado, bien, 
+ notable o sobresaliente.
+*/
+
+/*
+  Comprueba si dado un mes(Enero, Febrero...) y un día del mes (del 1 al 31), estamos en otoño, invierno, primavera o verano. 
+*/
+
+
+/*
+  Crea una función que categorice vehículos en función de sus características. Tendrá tres parámetros: 
+    - El número de ruedas
+    - El tipo de motor, debe ser gasolina, electrico o manual
+    - Si tiene pedales o no
+
+  Esta función debe devolver 'coche', 'moto', 'patinete', 'bicicleta' o 'desconocido':
+  - Los coches tienen 4 ruedas, no tienen pedales y pueden ser eléctricos o de gasolina.
+  - Las motos solo tienen 2 rueda, pueden ser eléctricas o de gasolina y no tienen pedales
+  - Las bicicletas solo tienen 2 ruedas y pueden ser eléctricas o manuales y tienen pedales.
+  - Los patinetes solo tienen 2 ruedas, pueden ser eléctricos o manuales y no tienen pedales pedales.
+  - En cualquier otro caso devuelve desconocido.
+*/
+
+
+
+
