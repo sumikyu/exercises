9f431da441001a3fd270b5e88c8e357fb6ae3ec1
Jose Miguel Gonzalez Garcia <josemigonzalez@metricool.com>
2023-11-19 12:18:09 +0100

    Initial vesrion of caza a josemi

---
+++ caza-al-murciano/index.js
@@ -0,0 +1,58 @@
+function startGame() {
+  const availableClasses = [
+    'josemi--t',
+    'josemi--b',
+    'josemi--corner-bl',
+    'josemi--corner-br',
+    'josemi--corner-tl',
+    'josemi--corner-tr'
+  ];
+  const availableTimers = [1000, 1200, 1500, 2000, 2500, 3000];
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function pickJosemi() {
+    if (!availableClasses.length) {
+      return;
+    }
+
+    const randomIndex = getRandomInt(0, availableClasses.length - 1);
+    const randomTimerIndex = getRandomInt(0, availableTimers.length - 1);
+    const timerToUse = availableTimers[randomTimerIndex];
+    const classToUse = availableClasses.splice(randomIndex, 1)[0];
+    const josemiNode = document.querySelector('.josemi:not(.josemi--selected)');
+
+    if (!josemiNode) {
+      return;
+    }
+
+    josemiNode.classList.add('josemi--selected');
+    josemiNode.classList.add(classToUse);
+
+    const clickListener = josemiNode.addEventListener('click', () => {
+      josemiNode.classList.remove('josemi--show');
+    });
+
+    setTimeout(() => {
+      josemiNode.classList.add('josemi--show');
+    }, 400);
+
+    setTimeout(() => {
+      josemiNode.classList.remove('josemi--show');
+
+      setTimeout(() => {
+        josemiNode.classList.remove('josemi--selected');
+        josemiNode.classList.remove(classToUse);
+        availableClasses.push(classToUse);
+        josemiNode.removeEventListener('click', clickListener);
+      }, 400);
+    }, timerToUse);
+  }
+
+  pickJosemi();
+  return setInterval(pickJosemi, 1000);
+}
